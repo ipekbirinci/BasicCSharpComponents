06e3e5cf5e0b4b17c63cdea25a32dfd73c4ad47f
ipek <152120191020@ogrenci.ogu.edu.tr>
2023-04-19 12:03:10 +0300

    Add TextBox, Buttons and another Windows Form page.

---
+++ Lab01/Lab01/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Lab01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxStudentId = new System.Windows.Forms.TextBox();
+            this.textBoxStudentName = new System.Windows.Forms.TextBox();
+            this.textBoxStudentSurname = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxStudentId
+            // 
+            this.textBoxStudentId.Location = new System.Drawing.Point(136, 12);
+            this.textBoxStudentId.MaxLength = 8;
+            this.textBoxStudentId.Name = "textBoxStudentId";
+            this.textBoxStudentId.Size = new System.Drawing.Size(125, 27);
+            this.textBoxStudentId.TabIndex = 0;
+            this.textBoxStudentId.Text = "20191020";
+            //this.textBoxStudentId.TextChanged += new System.EventHandler(this.textBoxStudentId_TextChanged);
+            this.textBoxStudentId.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxStudentId_KeyPress);
+            // 
+            // textBoxStudentName
+            // 
+            this.textBoxStudentName.Location = new System.Drawing.Point(136, 45);
+            this.textBoxStudentName.Name = "textBoxStudentName";
+            this.textBoxStudentName.Size = new System.Drawing.Size(125, 27);
+            this.textBoxStudentName.TabIndex = 1;
+            this.textBoxStudentName.Text = "ipek";
+            this.textBoxStudentName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxStudentName_KeyPress);
+            // 
+            // textBoxStudentSurname
+            // 
+            this.textBoxStudentSurname.Location = new System.Drawing.Point(136, 78);
+            this.textBoxStudentSurname.Name = "textBoxStudentSurname";
+            this.textBoxStudentSurname.Size = new System.Drawing.Size(125, 27);
+            this.textBoxStudentSurname.TabIndex = 2;
+            this.textBoxStudentSurname.Text = "birinci";
+            this.textBoxStudentSurname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxStudentSurname_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(136, 115);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(212, 138);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Lo1rem ip2sum do6lor si7t ame4t";
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(136, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "RUN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Lab01.Properties.Resources.chicken_icon;
+            this.pictureBox1.Image = global::Lab01.Properties.Resources.chicken_icon;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(421, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "StudentId(0-9):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name(A-Z):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Surname(A-Z):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Text:";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(136, 357);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(212, 177);
+            this.textBoxOutput.TabIndex = 10;
+            this.textBoxOutput.Text = "Results will show here!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Output->";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(600, 566);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBoxStudentSurname);
+            this.Controls.Add(this.textBoxStudentName);
+            this.Controls.Add(this.textBoxStudentId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStudentId;
+        private System.Windows.Forms.TextBox textBoxStudentName;
+        private System.Windows.Forms.TextBox textBoxStudentSurname;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab01/Lab01/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using testScript;
+
+namespace Lab01
+{
+   
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string studentId=textBoxStudentId.Text;
+            int studentNumber;
+            bool isNumeric = int.TryParse(studentId, out studentNumber);
+            string studentName = textBoxStudentName.Text;
+            string studentSurname = textBoxStudentSurname.Text;
+            string box = textBox4.Text;
+            string student;
+            int number = 0;
+            int letter = 0;
+            int score;
+            //Console.WriteLine(studentNumber + studentName+studentSurname+number+letter);
+            testClass test = new testClass();
+ 
+            //if (studentId.Trim() == "") return;
+            //if (studentName.Trim() == "") return;
+            //if (studentSurname.Trim() == "") return;
+            if (studentId.Length != 8) MessageBox.Show("Your number must involve 8 digits!!");
+            else
+            {
+              
+                foreach (char c in box)
+                {
+                    if (char.IsLetter(c))
+                    {
+                        letter++;
+                    }
+                    else if (char.IsDigit(c))
+                    {
+                        number++;
+                    }
+
+                }
+
+                score = test.testFunc(studentNumber, studentName, studentSurname, box, number, letter);
+                student = "Student number: " + studentId + Environment.NewLine + "First name: " + studentName + Environment.NewLine + "Last name: " + studentSurname + Environment.NewLine + "Quantity of letter of text: " + letter.ToString() + Environment.NewLine + "Quantity of number of text: "
+                + number.ToString() + Environment.NewLine + "Student score: " + score.ToString();
+
+                textBoxOutput.Text = student;
+              
+            }
+
+        }
+
+        private void textBoxStudentId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // ignore non-numeric characters
+            }
+        }
+
+        private void textBoxStudentName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+                e.Handled = true;
+            if (System.Text.Encoding.UTF8.GetByteCount(new char[] { e.KeyChar }) > 1)
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void textBoxStudentSurname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+                e.Handled = true;
+            if (System.Text.Encoding.UTF8.GetByteCount(new char[] { e.KeyChar }) > 1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
+            {
+                e.Handled = true; // ignore non-alphanumeric characters
+            }
+            
+        }
+
+    }
+}
